2e8ba6b1123b314ac140b0ddeac55515e321fab3
ValdezKankeu <vkankeu@gmail.com>
2025-09-03 23:32:34 -0400

    Co-authored-by: PetraFonjong <PetraFonjong@users.noreply.github.com>

--- public/script.js
+++ public/script.js
@@ -1,3 +1,5 @@
-function sayHello() {
-  alert("Hello from the frontend!");
-}
+const { response } = require("express");
+
+fetch("https://v6.exchangerate-api.com/v6/5ecd5d2bb673b49196b90730/latest/USD")
+.then(response => console.log(response))
+.catch(error => console.error(error))
